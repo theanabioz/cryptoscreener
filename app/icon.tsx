cc122f45bd32d82d204ef1257f99492f9f020164
Arsen Abdullaev <progear2008@gmail.com>
2026-01-19 03:32:34 +0200

    Chore: Add dynamic favicon

---
+++ app/icon.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: '#38B2AC',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '50%',
+        }}
+      >
+        C
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  )
+}
